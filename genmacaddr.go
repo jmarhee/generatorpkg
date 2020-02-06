b5eab93079a4006f5f100e7a7ceedb6b1551417c
Joseph D. Marhee <4262655-jmarhee@users.noreply.gitlab.com>
2020-02-06 07:15:05 +0000

    Revert "Update genmacaddr.go"
    This reverts commit 6ae449d3346a5969ffd5dd3d2e6792064d053e23

--- genmacaddr.go
+++ genmacaddr.go
@@ -19,5 +19,5 @@ func NewMacAddr() interface{} {
 	newMac := fmt.Sprintf("%x:%x:%x:%x:%x:%x",
 	    b[0], b[1], b[2], b[3], b[4], b[5])
 	newMacAddr := mac{MACAddr: newMac}
-	return _,newMacAddr.MACAddr
+	return newMacAddr.MACAddr
 }
